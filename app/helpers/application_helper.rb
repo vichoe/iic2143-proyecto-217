a8c23c20d247917d68d0c48d79ff275527c6cbb2
VICENTE DE GAVARDO <vicente.degavardo@estudiante.uc.cl>
2026-09-24 13:46:24 -0300

    foto de perfil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def avatar_for(user, size: 32)
+    style = "width: #{size}px; height: #{size}px; border-radius: 50%; vertical-align: middle;"
+
+    if user.avatar.attached?
+      image_tag user.avatar, alt: "Foto de #{user.name}", style: "#{style} object-fit: cover;"
+    else
+      tag.span user.name.first.upcase,
+               style: "#{style} display: inline-flex; align-items: center; justify-content: center; background: #ccc;"
+    end
+  end
 end
